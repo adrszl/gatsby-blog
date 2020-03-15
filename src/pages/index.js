d91228dee90533bf258b47e4508bc8d36aaec349
Adrian Szlegel <adrian@oaknode.com>
2020-03-15 21:09:22 +0100

    Setting up the project dependencies and structure

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <React.Fragment>
+            <h1>Gatsby blog project</h1>
+            <h2>Adrian Szlegel</h2>
+        </React.Fragment>
+    );
+};
