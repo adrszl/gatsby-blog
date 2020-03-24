380df0d20510bd0f100659770482e9ff58fe34e9
Adrian Szlegel <adrian@oaknode.com>
2020-03-24 21:54:32 +0100

    Added first styling to the index.js and used newly created hook for meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useSiteMetadata } from '../hooks/useSiteMetadata';
+import styled from 'styled-components';
+
+const StyledH1 = styled.h1`
+  color: rebeccapurple;
+`;
 
 export default () => {
+    const { title, description } = useSiteMetadata();
     return (
         <React.Fragment>
-            <h1>Gatsby blog project</h1>
+            <StyledH1>Gatsby blog project</StyledH1>
             <h2>Adrian Szlegel</h2>
+            <p>Meta:</p>
+            <p>Title: <code>{ title }</code></p>
+            <p>Description: <code>{ description }</code></p>
         </React.Fragment>
     );
 };
