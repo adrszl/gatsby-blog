a1156adb2a914dae033a5c41a369bfe5ec4e13d8
Adrian Szlegel <adrian@oaknode.com>
2020-04-05 13:12:18 +0200

    Code splitted to header and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { useSiteMetadata } from '../hooks/useSiteMetadata';
-import styled from 'styled-components';
-
-const StyledH1 = styled.h1`
-  color: rebeccapurple;
-`;
+import { Layout } from '../components/Layout';
 
 export default () => {
-    const { title, description } = useSiteMetadata();
     return (
         <React.Fragment>
-            <StyledH1>Gatsby blog project</StyledH1>
-            <h2>Adrian Szlegel</h2>
-            <p>Meta:</p>
-            <p>Title: <code>{ title }</code></p>
-            <p>Description: <code>{ description }</code></p>
+            <Layout />
         </React.Fragment>
     );
 };
