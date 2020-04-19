7c79d547e6200cc4bd7cf1dc5a72fe2449f50544
Adrian Szlegel <adrian.szlegel@hotmail.com>
2020-04-19 16:25:27 +0200

    Added site index query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { Layout } from '../components/Layout';
 
-export default () => {
+export default ({ data }) => {
     return (
         <React.Fragment>
-            <Layout />
+            <Layout>
+                {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+                    <React.Fragment>
+                        <h1>{frontmatter.title}</h1>
+                        <p>{frontmatter.date}</p>
+                        <p>{excerpt}</p>
+                    </React.Fragment>
+                ))}
+            </Layout>
         </React.Fragment>
     );
 };
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
